43f61ce1498d5a46f57109a494f76ed12658d5e3
Farhan Nasiruddeen <farnas2016@gmail.com>
2024-10-01 16:25:55 +0100

    Landing pages added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import LandingPage from "./(components)/LandingPage";
 import LinkWalletButton from "./(components)/LinkWalletButton";
 
 export default function Home() {
   return (
     <div>
       <LinkWalletButton />
+      <LandingPage />
     </div>
   );
 }
