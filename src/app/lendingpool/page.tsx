1d3b8f17597f41c9f87a15b50d833d2ecb05dd0b
Aditya Pundir <pundir.aditya@outlook.com>
2024-10-04 21:38:00 +0530

    lending pool stats

--- src/app/lendingpool/page.tsx
+++ src/app/lendingpool/page.tsx
@@ -1,21 +1,9 @@
 "use client";
-import { FeatureCards } from "@/components/FeatureCard";
 import React, { useState } from "react";
+import { FeatureCards } from "./utils";
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 
-export const cards = [
-  {
-    title: "Borrow",
-    src: "https://www.pexels.com/photo/an-artist-s-illustration-of-artificial-intelligence-ai-this-image-represents-ethics-research-understanding-human-involvement-in-data-labelling-it-was-created-by-ariel-lu-as-part-of-the-18068746/",
-  },
-  {
-    title: "Invest",
-    src: "/images/invest.png",
-  },
-  {
-    title: "FlashLoan",
-    src: "/images/flash-loan.jpg",
-  },
-];
+
 
 const page = () => {
   const [contractBalance, setContractBalance] = useState(0);
@@ -26,8 +14,13 @@ const page = () => {
     <div className="min-h-[100vh] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex flex-col gap-5 md:gap-10 justify-center">
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      <div className="mt-24 text-5xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8 text-center">
+      <div className="mt-20 md:mt-0 text-5xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8 text-center">
         Lending Pool
+      </div>
+      <div className="px-4 md:px-[6rem]">
+        <TextGenerateEffect words={`Current Contract Balance is ${contractBalance} TRX` } />
+        {amountInTRX && <TextGenerateEffect words={`Current Contract Balance is ${contractBalance}`} />}
+        {amountInJST && <TextGenerateEffect words={`Current Contract Balance is ${contractBalance}`} />}
       </div>
       <div className="px-4 md:px-[6rem]">
         <FeatureCards />
