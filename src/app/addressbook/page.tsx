ed0ab3315551c77db6d3b1a4a74c8100d26757cc
Jaskaran Singh <jaskaransingh@Jaskarans-Laptop.local>
2024-10-09 02:33:17 +0530

    minor

--- src/app/addressbook/page.tsx
+++ src/app/addressbook/page.tsx
@@ -5,7 +5,7 @@ function AddressBook() {
     <div className="bg-white dark:bg-black text-black dark:text-white w-full h-full">
       <div className="text-5xl md:text-7xl my-[4rem] mb-1 px-2 md:px-4 flex justify-center w-full text-center font-bold relative  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
         <p className="max-w-4xl text-wrap px-4">
-          Address Book
+          Address Book Deployed on Nile
         </p>
       </div>
       <div className="text-lg md:text-2xl my-[2rem] flex justify-center w-full font-bold relative bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-4">
@@ -29,7 +29,7 @@ function AddressBook() {
         </p>
       </div>
     </div>
-  )
+  );
 }
 
 export default AddressBook;
